7ab9b2767dab4c521f36d7e3ed85c1a54164f5f0
amir <jramir669@gmail.com>
2020-03-15 09:46:35 -0700

    finish user profile

--- src/components/Editable/Editable.js
+++ src/components/Editable/Editable.js
@@ -1,12 +1,34 @@
 import React from "react";
+export const EditContext = React.createContext(null);
 
-export default React.forwardRef(function(
-  { text, as: Component = <input />, onSuccess, onCancle, ...props },
+const Editable = function(
+  { text, onSuccess, onCancel, children, ...props },
   ref
 ) {
   const [editMode, setEditMode] = React.useState(false);
+  const toggle = () => setEditMode(state => !state);
+
+  return (
+    <EditContext.Provider
+      value={{ toggle, editMode, text, onSuccess, onCancel }}
+    >
+      {children}
+    </EditContext.Provider>
+  );
+};
+
+function Toggle({ as: Component = "div", ...props }) {
+  const { toggle } = React.useContext(EditContext);
+  return <Component {...props} onClick={toggle} />;
+}
+const View = React.forwardRef(function View(
+  { Input = "input", Content = "div", wrapperClassName = "", ...props },
+  ref
+) {
+  const { editMode, toggle, text, onSuccess, onCancel } = React.useContext(
+    EditContext
+  );
   const [input, setInput] = React.useState(text);
-  const toggle = setEditMode(state => !state);
 
   function EditView() {
     const inputProps = {
@@ -16,33 +38,34 @@ export default React.forwardRef(function(
     };
     const handleSubmit = function(e) {
       e.preventDefault();
-      onSuccess(input);
+      typeof onSuccess === "function" && onSuccess(input);
       toggle();
     };
     const handleCancel = function(e) {
       console.log("cancel");
       setInput(text);
+      typeof onCancel === "function" && onCancel();
       toggle();
     };
     return (
-      <form onSubmit={handleSubmit}>
-        <Component
-          {...inputProps}
-          ref={ref}
-          defaultValue={text}
-          value={input}
-        />
-        <button type="submit">change</button>
-        <button onClick={handleCancel}>cancel</button>
+      <form onSubmit={handleSubmit} className={wrapperClassName}>
+        <Input {...inputProps} ref={ref} value={input} autoFocus />
+        <button type="submit">
+          <span className="lnr-checkmark-circle lnr" />
+        </button>
+        <button onClick={handleCancel}>
+          <span className="lnr-cross lnr" />
+        </button>
       </form>
     );
   }
   function NormalView() {
-    return (
-      <div {...props} onClick={toggle}>
-        {text}
-      </div>
-    );
+    return <Content {...props}>{text}</Content>;
   }
   return editMode ? <EditView /> : <NormalView />;
 });
+
+Editable.Toggle = Toggle;
+Editable.View = View;
+
+export default Editable;
--- src/components/ProfileItem/ProfileItem.js
+++ src/components/ProfileItem/ProfileItem.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./ProfileItem.styles.scss";
+import Editable from "../Editable/Editable";
 
 export default function({ icon, label, editable, value }) {
   return (
-    <div className="profile__item" disabled={!editable}>
-      <span className={`profile__item__icon lnr ${icon}`} />
-      <span className="profile__item__label">{label}</span>
-      <span className="profile__item__value">{value}</span>
-      {editable && (
-        <span className="lnr lnr-pencil cursor--pointer profile-edit-pencil" />
-      )}
-    </div>
+    <Editable text={value} onSuccess={input => console.log("input", input)}>
+      <div className="profile__item" disabled={!editable}>
+        <span className={`profile__item__icon lnr ${icon}`} />
+        <span className="profile__item__label">{label}</span>
+        <Editable.View
+          className="profile__item__value"
+          Content="span"
+          wrapperClassName="change-input-profile"
+        />
+        {editable && (
+          <Editable.Toggle
+            as="span"
+            className="lnr lnr-pencil cursor--pointer profile-edit-pencil"
+          />
+        )}
+      </div>
+    </Editable>
   );
 }
