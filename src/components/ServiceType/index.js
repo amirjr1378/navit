fa2db8e2cc35a92f0e8cb3674d57e882b9ec6a83
amir <jramir669@gmail.com>
2020-03-19 04:50:17 -0700

    make performance optimizition

--- src/components/ServiceType/index.js
+++ src/components/ServiceType/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import classnames from "classnames";
 import "./ServiceType.styles.scss";
 
-function ServiceType({ icon, label, color }) {
+function ServiceType({ className, icon, label, color, ...props }) {
+  console.log("rerender service type");
   return (
-    <div className={classnames("service-type__container", { [color]: color })}>
+    <div
+      className={classnames("service-type__container", {
+        [color]: color,
+        [className]: className
+      })}
+      {...props}
+    >
       <span className={`service-type__icon lnr ${icon}`} />
 
       <div className="service-type__line">
--- src/pages/ServicesSelected/index.js
+++ src/pages/ServicesSelected/index.js
@@ -14,43 +14,48 @@ const darkUrl =
 
 const streetmap = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
-function ServicesSelected(props) {
-  const [viewport, setViewport] = useState({
-    center: [35.6961, 51.4231],
-    zoom: 19
-  });
-  return (
-    <div className="services">
-      <div style={{ direction: "ltr", width: "100%", height: "100vh" }}>
-        <Map
-          viewport={viewport}
-          onViewportChange={newViewport => setViewport(newViewport)}
-        />
-      </div>
-      <div className="services__header">
-        <Dropdown>
-          <Toggle as={"div"}>
-            <Button circle color="warning-outline" style={{ padding: 5 }}>
-              <Menu />
-            </Button>
-          </Toggle>
-          <Content>
-            <span name="item1">items</span>
-            <span name="item2">items</span>
-            <span name="item 3">items</span>
-          </Content>
-        </Dropdown>
-        <Button circle color="warning-outline">
-          <span className="lnr lnr-arrow-left" />
-        </Button>
-      </div>
+class ServicesSelected extends React.Component {
+  state = {
+    viewport: {
+      center: [35.6961, 51.4231],
+      zoom: 19
+    }
+  };
+  render() {
+    const { viewport } = this.state;
+    return (
+      <div className="services">
+        <div style={{ direction: "ltr", width: "100%", height: "100vh" }}>
+          <Map
+            viewport={viewport}
+            onViewportChange={viewport => this.setState({ viewport })}
+          />
+        </div>
+        <div className="services__header">
+          <Dropdown>
+            <Toggle as={"div"}>
+              <Button circle color="warning-outline" style={{ padding: 5 }}>
+                <Menu />
+              </Button>
+            </Toggle>
+            <Content>
+              <span name="item1">items</span>
+              <span name="item2">items</span>
+              <span name="item 3">items</span>
+            </Content>
+          </Dropdown>
+          <Button circle color="warning-outline">
+            <span className="lnr lnr-arrow-left" />
+          </Button>
+        </div>
 
-      <div className="services__footer">
-        <ServiceType icon="lnr-calendar-full" label="درخواست سرویس" />
-        <ServiceType icon="lnr-car" label="درخواست تاکسی" color="warning" />
+        <div className="services__footer">
+          <ServiceType icon="lnr-calendar-full" label="درخواست سرویس" />
+          <ServiceType icon="lnr-car" label="درخواست تاکسی" color="warning" />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ServicesSelected;
