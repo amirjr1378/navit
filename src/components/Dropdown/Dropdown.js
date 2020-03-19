c3a18fb167ea9782372a4188e3aec2f65e4895a3
amir <jramir669@gmail.com>
2020-03-19 04:11:50 -0700

    drop down component

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,43 @@
+import React from "react";
+import classnames from "classnames";
+import proptypes from "prop-types";
+import "./Dropdown.styles.scss";
+function Dropdown({ children, as: Component = "div", className, ...props }) {
+  return (
+    <Component
+      className={classnames("dropdown__container", { [className]: className })}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+}
+export const Toggle = React.memo(function({ className, children }) {
+  return React.Children.map(children, child =>
+    React.cloneElement(child, {
+      className: classnames("dropdown__button", child.props.className)
+    })
+  );
+});
+Toggle.propTypes = {
+  className: proptypes.string,
+  children: proptypes.element.isRequired
+};
+
+export const Content = React.memo(function({
+  children,
+  className,
+  as: Component = "div",
+  ...props
+}) {
+  return (
+    <Component className="dropdown__content" {...props}>
+      {children}
+    </Component>
+  );
+});
+
+Content.propTypes = {
+  className: proptypes.string
+};
+export default React.memo(Dropdown);
