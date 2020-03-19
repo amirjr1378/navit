1b7ebe8f13fc2b88f9590d588ed461885771c160
amir <jramir669@gmail.com>
2020-03-19 04:12:14 -0700

    abstract map now when we want change map we just manipulate map component

---
+++ src/components/Map/Leaflet/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+
+const token =
+  "pk.eyJ1IjoiYW1pcnNuYWtlIiwiYSI6ImNrN3hneG44ajBjOTgzZXFwenUzbjRuN2UifQ.lWPO3NBswNdXgnSGv02QlA";
+const lightUrl =
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png";
+const darkUrl =
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
+
+const streetmap = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+function Map({ children, ...props }) {
+  return (
+    <LeafletMap {...props}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url={streetmap}
+      />
+      {children}
+    </LeafletMap>
+  );
+}
+
+// export default React.memo(Map);
+export default Map;
--- src/pages/ServicesSelected/index.js
+++ src/pages/ServicesSelected/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { Map } from "../../components/Map";
 import { Menu } from "../../components/Icons";
 import { Button } from "../../components/Button";
 import "./ServicesSelected.styles.scss";
 import ServiceType from "../../components/ServiceType";
+import Dropdown, { Toggle, Content } from "../../components/Dropdown/Dropdown";
 const token =
   "pk.eyJ1IjoiYW1pcnNuYWtlIiwiYSI6ImNrN3hneG44ajBjOTgzZXFwenUzbjRuN2UifQ.lWPO3NBswNdXgnSGv02QlA";
 const lightUrl =
@@ -24,17 +25,21 @@ function ServicesSelected(props) {
         <Map
           viewport={viewport}
           onViewportChange={newViewport => setViewport(newViewport)}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url={streetmap}
-          />
-        </Map>
+        />
       </div>
       <div className="services__header">
-        <Button circle color="warning-outline" style={{ padding: 5 }}>
-          <Menu />
-        </Button>
+        <Dropdown>
+          <Toggle as={"div"}>
+            <Button circle color="warning-outline" style={{ padding: 5 }}>
+              <Menu />
+            </Button>
+          </Toggle>
+          <Content>
+            <span name="item1">items</span>
+            <span name="item2">items</span>
+            <span name="item 3">items</span>
+          </Content>
+        </Dropdown>
         <Button circle color="warning-outline">
           <span className="lnr lnr-arrow-left" />
         </Button>
