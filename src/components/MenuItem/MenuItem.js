f81bb69d753f55961a2392059cc043537bffcf1e
amir <jramir669@gmail.com>
2020-03-20 04:46:04 -0700

    add badge for menu item

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./MenuItem.styles.scss";
 
-export default function({ icon, label }) {
+export default function({ icon, label, badge }) {
   return (
     <div className="menu__item">
       <span className={"menu__item__icon lnr " + icon} />
       <span className="menu__item__label">{label}</span>
+      {badge && <div className="menu__item__badge">{badge} </div>}
     </div>
   );
 }
--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -27,14 +27,14 @@ function Header(props) {
 }
 
 const menuItems = [
-  { label: "سفر ها", icon: "lnr-heart" },
+  { label: "سفر ها", icon: "lnr-car" },
   { label: "سرویس ها", icon: "lnr-briefcase" },
-  { label: "افزایش اعتبار", icon: "lnr-heart" },
+  { label: "افزایش اعتبار", icon: "lnr-heart", badge: 2 },
   { label: "تراکنش ها", icon: "lnr-pie-chart" },
   { label: "منتخب ها", icon: "lnr-star" },
   { label: "اطلاعات کاربری", icon: "lnr-user" },
   { label: "درباره ما", icon: "lnr-heart" },
-  { label: "پشتیبانی", icon: "lnr-bubble" },
+  { label: "پشتیبانی", icon: "lnr-bubble", badge: 5 },
   { label: "سوالات متداول", icon: "lnr-question-circle" },
   { label: "تنظیمات", icon: "lnr-cog" }
 ];
